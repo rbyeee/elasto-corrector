e5b724af4316e03b0a592c01d664142f584b9278
Egor Novikov <42256950+rbyea@users.noreply.github.com>
2026-03-21 17:40:52 +0300

    21.03

--- app/js/db/script.js
+++ app/js/db/script.js
@@ -1,3 +1,15 @@
 $(".smile__item-height").matchHeight({
 	byRow: true,
 })
+
+$(".tehnology__item-height").matchHeight({
+	byRow: true,
+})
+
+$(".perfect__descript-height").matchHeight({
+	byRow: true,
+})
+
+$(".will-do__description-height").matchHeight({
+	byRow: true,
+})
